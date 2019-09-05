040b8145c92fd0856f5b1326805160d86786d77e
Paul Kim <paul@terra.money>
2019-09-05 17:26:51 +0900

    Added bcryptjs to bcrypt benchmark Added compare stage to bcrypt benchmark

--- bcrypt/index.js
+++ bcrypt/index.js
@@ -1,18 +1,37 @@
 const bcrypt = require('bcrypt');
+const bcryptjs = require('bcryptjs');
 const { Suite } = require('benchmark');
-const suite = new Suite('bcrypt', { minTime: 10000 });
+const suite = new Suite('bcrypt', { minTime: 30000 });
 
 const myPlaintextPassword = '101010';
+let h;
 
 suite
-  .add('salt round 10', () => {
+  .add('hash round 1', () => {
+    bcrypt.hashSync(myPlaintextPassword, 1);
+  })
+  .add('hash round 3', () => {
+    bcrypt.hashSync(myPlaintextPassword, 3);
+  })
+  .add('hash round 5', () => {
+    bcrypt.hashSync(myPlaintextPassword, 5);
+  })
+  .add('hash round 10', () => {
     bcrypt.hashSync(myPlaintextPassword, 10);
   })
-  .add('salt round 11', () => {
+  .add('hash round 11', () => {
     bcrypt.hashSync(myPlaintextPassword, 11);
   })
-  .add('salt round 12', () => {
+  .add('hash round 12', () => {
     bcrypt.hashSync(myPlaintextPassword, 12);
+  })
+  .add('js hash round 10', () => {
+    h = bcryptjs.hashSync(myPlaintextPassword, 10);
+  })
+  .add('compare', () => {
+    if (!bcrypt.compareSync(myPlaintextPassword, h)) {
+      console.log('wtf');
+    }
   })
   .on('cycle', function(event) {
     console.log(String(event.target));
